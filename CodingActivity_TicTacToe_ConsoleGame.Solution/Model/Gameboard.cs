03e8c3d9d506a6f193da9220cc60681d16f1a71f
lavoiee <ericjohnlavoie@gmail.com>
2017-09-19 17:34:41 -0400

    Changed MAX_NUM_OF_ROWS_COLUMNS to 4

--- CodingActivity_TicTacToe_ConsoleGame.Solution/Model/Gameboard.cs
+++ CodingActivity_TicTacToe_ConsoleGame.Solution/Model/Gameboard.cs
@@ -34,7 +34,7 @@ namespace CodingActivity_TicTacToe_ConsoleGame
 
         #region FIELDS
 
-        private const int MAX_NUM_OF_ROWS_COLUMNS = 3;
+        private const int MAX_NUM_OF_ROWS_COLUMNS = 4;
 
         private PlayerPiece[,] _positionState;
 
